fb23684c804e765d136cb821295ba761fd8806b3
Manuel C Solis <manuelsolis@Manuels-Air.localdomain>
2018-09-27 17:48:59 -0400

    Added react-router-dom, created 3 components

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import InterestingQuestions from "./Components/InterestingQuestions";
 import TopQuestions from "./Components/TopQuestions";
 import HotQuestions from "./Components/HotQuestions";
@@ -11,37 +11,37 @@ class App extends Component {
   render() {
     return (
       <Router>
-      <div>
-      <section className="App">
-        <section className="App-header">
-          <img
-            src="./images/stack-overflow.png"
-            className="App-logo"
-            alt="logo"/>
-          <input type="text" placeholder="Search"></input>
-          <button>Submit</button>
-        </section>
-        </section>
+        <div>
+          <section className="App">
+            <section className="App-header">
+              <img
+                src="./images/stack-overflow.png"
+                className="App-logo"
+                alt="logo" />
+              <input type="text" placeholder="Search"></input>
+              <button>Submit</button>
+            </section>
+          </section>
           <Switch>
             <Route path="/InterestingQuestions" exact Component={InterestingQuestions} />
-            <Route path="/TopQuestions" exact Component={TopQuestions}/>
-            <Route path="/HotQuestions" exact Component={HotQuestions}/>
+            <Route path="/TopQuestions" exact Component={TopQuestions} />
+            <Route path="/HotQuestions" exact Component={HotQuestions} />
           </Switch>
-      <section className="App">
-          <h2>Type in a title</h2>
-          <input type="text" placeholder="Title" />
-          <p>Type in your question</p>
-          <textarea name="Question" />
-          <h2>Type in your answer</h2>
-          <textarea height="200" width="600" name="Question" />
-          <br />
-          <br />
-        <section>
-          <button>Up Vote</button>
-          <button>Down Vote</button>
-        </section>
-        </section>
-      </div>
+          <section className="App">
+            <h2>Type in a title</h2>
+            <input type="text" placeholder="Title" />
+            <p>Type in your question</p>
+            <textarea name="Question" />
+            <h2>Type in your answer</h2>
+            <textarea height="200" width="600" name="Question" />
+            <br />
+            <br />
+            <section>
+              <button>Up Vote</button>
+              <button>Down Vote</button>
+            </section>
+          </section>
+        </div>
       </Router>
     );
   }
