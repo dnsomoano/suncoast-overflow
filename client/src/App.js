3459df4e5dd9b23636781bf936a7a8cd48103311
Manuel C Solis <manuelsolis@Manuels-Air.localdomain>
2018-09-27 16:34:52 -0400

    html and header done. Ready to install react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import {BrowserRouter as Router} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro"></p>
-      <form>
-      <span>
-        <h2>Type in a title</h2><input type="text" placeholder="Title"></input>
-      </span>
-      <p>Type in your question</p>
-      <textarea
-        height="200"
-        width="600"
-        name="Question"/>
-      <p>Type in your answer</p>
-      <textarea
-        height="200"
-        width="600"
-        name="Question"/>  
-        <br></br>
-        <br></br>
-      <section>
-      <button>Up Vote</button>
-      <button>Down Vote</button>
-      </section>
-      </form>
+      <div>
+        <section className="App-header">
+          <img
+            src="./images/stack-overflow.png"
+            className="App-logo"
+            alt="logo"/>
+          <input type="text" placeholder="Search"></input>
+          <button>Submit</button>
+        </section>
+        <section className="App">
+          <h2>Type in a title</h2>
+          <input type="text" placeholder="Title" />
+          <p>Type in your question</p>
+          <textarea name="Question" />
+          <h2>Type in your answer</h2>
+          <textarea height="200" width="600" name="Question" />
+          <br />
+          <br />
+        <section>
+          <button>Up Vote</button>
+          <button>Down Vote</button>
+        </section>
+        </section>
       </div>
     );
   }
 }
 
 export default App;
-
