deb5fea3d726cd6d430fe11a82e4b1a778e872e1
Daniel N Somoano <dnsomoano@gmail.com>
2018-09-28 22:42:44 -0400

    Working on fetch, committing before storm. Must extract values from array, then display.

--- client/src/Components/TopQuestions.js
+++ client/src/Components/TopQuestions.js
@@ -1,13 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class TopQuestions extends Component {
-    render() {
-        return (
-            <div>
-                These are the Top Questions Developers are asking underflow
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [{}],
+      upVote: 0,
+      downVote: 0,
+      title: "",
+      body: "",
+      date: new Date(),
+      user: ""
+    };
+  }
+  componentDidMount() {
+    fetch("https://localhost:5001/api/questions", {
+      //   mode: "no-cors"
+    })
+      .then(resp => resp.json())
+      .then(questions => {
+        console.log(questions);
+        this.setState({
+          data: questions
+        });
+      });
+  }
+
+  render() {
+    return (
+      <section>
+        <div>These are the Top Questions Developers are asking underflow</div>
+        {/* <section>
+          {this.state.data.map((questions, i) => {
+            return <section key={i}>{questions}</section>;
+          })}
+        </section> */}
+      </section>
+    );
+  }
 }
 
 export default TopQuestions;
