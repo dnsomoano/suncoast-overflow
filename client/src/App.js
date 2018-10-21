014f4f63a1cfe4f56ae4b43b0c3964f295335982
Daniel N Somoano <dnsomoano@gmail.com>
2018-10-21 04:57:27 -0400

    Added routes and styling to app.js.

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,15 @@ import React, { Component } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import InterestingQuestions from "./Components/InterestingQuestions";
-import TopQuestions from "./Components/TopQuestions";
+import QuestionDetail from "./Components/QuestionDetail";
+import AskQuestion from "./Components/AskQuestion";
 import HotQuestions from "./Components/HotQuestions";
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div>
+        <div className="body">
           <section className="App">
             <section className="App-header">
               <img
@@ -21,35 +22,39 @@ class App extends Component {
               <button>Submit</button>
             </section>
           </section>
-          <section className="Nav-bar">
-            <h2>Top Questions</h2>
-            <button>Ask Question</button>
-            <section />
-            <ul className="Questions-nav-bar-ul">
-              <li>
-                <Link to="/">
-                  <button type="button">Home</button>
-                </Link>
-              </li>
-              <li>
-                <Link to="/InterestingQuestions">
-                  <button type="button">Interesting</button>
-                </Link>
-              </li>
-              <li>
-                <Link to="/HotQuestions">
-                  <button type="button">Hot</button>
-                </Link>
-              </li>
-            </ul>
+          <section className="main-header-container">
+            <section className="header-container">
+              <h1>Top Questions</h1>
+              <Link to="/questions/ask">
+                <button className="ask-question">Ask Question</button>
+              </Link>
+            </section>
+            <section className="Nav-bar">
+              <ul className="questions-breadcrumbs">
+                <li>
+                  <Link to="/">
+                    <button type="button">Interesting</button>
+                  </Link>
+                </li>
+                <li>
+                  <button type="button">Featured</button>
+                </li>
+                <li>
+                  <Link to="/HotQuestions">
+                    <button type="button">Hot</button>
+                  </Link>
+                </li>
+              </ul>
+            </section>
           </section>
           <Switch>
-            <Route path="/" exact component={TopQuestions} />
+            <Route path="/" exact component={InterestingQuestions} />
             <Route
-              path="/InterestingQuestions"
+              path="/questions/:id/:title"
               exact
-              component={InterestingQuestions}
+              component={QuestionDetail}
             />
+            <Route path="/questions/ask" exact component={AskQuestion} />
             <Route path="/HotQuestions" exact component={HotQuestions} />
           </Switch>
           <section className="App" />
